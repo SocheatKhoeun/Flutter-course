d0dce3eaf4f7814cf9b4a69cc8cbae90106928f7
SocheatKhoeun <159861545+SocheatKhoeun@users.noreply.github.com>
2024-10-12 21:27:22 +0700

    dart_s2

--- W1-S2-PRACTICE-Dart/EX-1.dart
+++ W1-S2-PRACTICE-Dart/EX-1.dart
@@ -1,1 +1,5 @@
 // Answer on doc file
+void main(){
+
+  
+}
--- W1-S2-PRACTICE-Dart/EX-3.dart
+++ W1-S2-PRACTICE-Dart/EX-3.dart
@@ -1,7 +1,16 @@
 void main() {
   // List of student scores
-  List<int> scores = [45, 67, 82, 49, 51, 78, 92, 30];
+  List<int> scores = [45, 78, 62, 49, 85, 33, 90, 50];
 
-  // You code
- 
+
+  var passed = scores.where((scores) => scores >= 50);
+
+  //Output the original scores 
+  print(scores);
+
+  //let passedcount variale for count the scores that higher than 50 
+  int passedCount = passed.length;
+  
+  print(passed.toList());
+  print('$passedCount students pasesd');
 }
