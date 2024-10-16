bd963fe0c7540fd90698f930676073b95b09ce02
SocheatKhoeun <159861545+SocheatKhoeun@users.noreply.github.com>
2024-10-16 17:41:49 +0700

    Dart

--- Challenges/level4.dart
+++ Challenges/level4.dart
@@ -1,31 +1,28 @@
 class Distance {
-  final double kms;
-  final double meters;
-  final double cms;
+  final double meters; 
+  // Constructors
+  Distance.cms(double cms): meters = cms / 100;
+  Distance.meters(double meters): meters = meters;
+  Distance.kms(double kms): meters = kms * 1000;
 
-  // Named uctors
-  Distance.kms(this.kms): 
-    meters = kms * 1000, 
-    cms = kms * 100000;
-  Distance.meters(this.meters): 
-    kms = meters / 1000, 
-    cms = meters * 100;
+  // methods
+  double get cms => meters * 100;
+  double get kms => meters / 1000;
 
-  // Operator Overloading
+  // Operator Overloading for addition
   Distance operator +(Distance other) {
-    // We can also use the unit cms or kms
-    return Distance.meters(meters + other.meters); 
+    return Distance.meters(this.meters + other.meters);
   }
 }
 
 void main() {
-  Distance d1 = Distance.kms(3.4);
-  Distance d2 = Distance.meters(1000);
-  
-  print(d1.meters);       
-  print(d2.kms);   
-  print((d1 + d2).kms); 
-  print((d1 + d2).meters);
-  print((d1 + d2).cms);
+  Distance d1 = Distance.kms(3.4);   
+  Distance d2 = Distance.meters(1000); 
 
+  // Display values
+  print(d1.kms);       
+  print(d2.kms);
+  print((d1 + d2).kms);
+  print((d1 + d2).meters); 
+  print((d1 + d2).cms);
 }
