ec5e1a09827a3e5eb17dc23f838d485c015c43d5
SocheatKhoeun <159861545+SocheatKhoeun@users.noreply.github.com>
2024-10-29 20:11:47 +0700

    Flutter

--- W3-S2/EX-3/main.dart
+++ W3-S2/EX-3/main.dart
@@ -1,65 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Container(
-      color: Colors.grey,
-      padding: const EdgeInsets.all(40),   
-    child: Center(
-      child: Column(
-        children: [
-          Container(
-              padding: const EdgeInsets.fromLTRB(200, 50, 200, 50),
-              margin: const EdgeInsets.symmetric(vertical: 20),
-              decoration: BoxDecoration(
-                  color: Colors.blue[100],
-                  borderRadius: BorderRadius.circular(30)),
-              child: const Center(
-                child: Text(
-                  "OOP",
-                  style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.none, 
-                      fontSize: 20),
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Center( 
+          child: Container(
+            padding: const EdgeInsets.all(20), 
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20), 
+                  child: _GradientContainer('OOP', Colors.blue[100]!),
                 ),
-              )),
-          Container(
-              padding: const EdgeInsets.fromLTRB(200, 50, 200, 50),
-              margin: const EdgeInsets.symmetric(vertical: 20),
-              decoration: BoxDecoration(
-                  color: Colors.blue[300],
-                  borderRadius: BorderRadius.circular(30)),
-              child: const Center(
-                child: Text(
-                  "DART",
-                  style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.none,
-                      fontSize: 20),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  child: _GradientContainer('DART', Colors.blue[300]!),
                 ),
-              )),
-          Container(
-              padding: const EdgeInsets.fromLTRB(200, 50, 200, 50),
-              margin: const EdgeInsets.symmetric(vertical: 20),
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Colors.blue,
-                    Colors.black,
-                  ],
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  child: _GradientContainer('FLUTTER', Colors.blue[600]!),
                 ),
-                  borderRadius: BorderRadius.circular(30)),
-              child: const Center(
-                child: Text(
-                  "FLUTTER",
-                  style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.none,  
-                      fontSize: 20),
-                ),
-              )),
-        ],
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  )));
+    );
+  }
+
+  Widget _GradientContainer(String text, Color color) {
+    return Container(
+      width: 900,
+      height: 50,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: Text(
+          text,
+          textAlign: TextAlign.center, 
+          style: const TextStyle(
+            fontSize: 35,
+            color: Colors.white, 
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
 }
--- W3-S2/EX-4/main.dart
+++ W3-S2/EX-4/main.dart
@@ -1,52 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Container(
-      color: Colors.grey,
-      padding: const EdgeInsets.all(40),   
-    child: Center(
-      child: Column(
-            children: [
-              CustomCard(text: "OOP", color: Colors.blue[100]),
-              CustomCard(text: "DART", color: Colors.blue[300]),
-              CustomCard(text: "FLUTTER", color: Colors.blue[600]) 
-            ],
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CustomCard(
+                  text: 'OOP',
+                  color: Colors.blue[100]!,
+                ),
+                CustomCard(
+                  text: 'DART',
+                  color: Colors.blue[300]!,
+                ),
+                CustomCard(
+                  text: 'FLUTTER',
+                  color: Colors.blue[600]!,
+                ),
+              ],
+            ),
           ),
         ),
- 
-      )
-        
       ),
     );
+  }
 }
 
 class CustomCard extends StatelessWidget {
   final String text;
   final Color? color;
+  final Gradient? gradient;
 
   const CustomCard({
-    required this.text,
-    this.color = Colors.blue,
     super.key,
+    required this.text,
+    this.color,
+    this.gradient,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.fromLTRB(200, 50, 200, 50),
       margin: const EdgeInsets.symmetric(vertical: 20),
+      width: 900,
+      height: 50,
       decoration: BoxDecoration(
         color: color,
-        borderRadius: BorderRadius.circular(30),
+        gradient: gradient,
+        borderRadius: BorderRadius.circular(20),
       ),
       child: Center(
         child: Text(
-          text,
+          text,  
+          textAlign: TextAlign.center,
           style: const TextStyle(
+            fontSize: 35,
             color: Colors.white,
-            decoration: TextDecoration.none, 
-            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
